5c879d332951eea348b1309d5d5fc22ea7880061
tranzert <gonzalo.tonanes@gmail.com>
2020-11-16 19:58:42 -0300

    correciones

--- block.js
+++ block.js
@@ -9,7 +9,7 @@ class Block{
         this.hash= this.createHash();
         this.nonce=0;
     }
-
+    //funcion para crear el hash
     createHash(){
         return SHA256(this.index + this.date + this.data +this.previosHash +this.nonce).toString();
     }
@@ -35,9 +35,10 @@ class BlockChain{
         return this.chain[lastPosition];
     }
     addBlock(data){
+        //Se obtiene el bloque anterior y su hash, luego el indice +1 para crear un bloque nuevo
         let prevBlock = this.getLastBlock();
-        let newIndex= prevBlock.index+1;
         let prevHash= prevBlock.hash;
+        let newIndex= prevBlock.index+1;
         let block= new Block(newIndex,data,prevHash);
         block.mine(this.difficulty);
         console.log('minado '+block.nonce+' con nonce '+block.nonce);
@@ -49,24 +50,36 @@ class BlockChain{
         for (let i = 1; i < this.chain.length; i++) {
             let prevBlock= this.chain[i-1];
             let currentBlock= this.chain[i];
-
+            // comprueba si el hash previo del bloque actual es diferente del hash del bloque anterior
             if (currentBlock.previosHash != prevBlock.hash) {
                 return false;
             }
+            //comprueba de los hash son iguales del bloque actual
             if (currentBlock.createHash() !=currentBlock.hash) {
                 return false;
             }
-            return true;
-
-            
+            return true; 
         }
     }
 }
-let Redes2Coin = new  BlockChain('info de genesis','000');
 
-Redes2Coin.addBlock('Valgo 32k');
-Redes2Coin.addBlock('Valgo 64k');
-//console.log(Redes2Coin.isValid());
-//Redes2Coin.chain[1].data ='fake data';
-//console.log(Redes2Coin.isValid());
-//console.log(JSON.stringify(Redes2Coin,null,2)); 
+
+//CREACIÓN DE LA CADENA DE BLOQUES
+let Redes2Coin = new  BlockChain('info de genesis','00');
+
+
+//SE AGREGAN 2 BLOQUES A LA CADENA CON SUS RESPECTIVOS DATOS
+Redes2Coin.addBlock('Datos de bloque 1');
+Redes2Coin.addBlock('Datos de bloque 2');
+//IMPRIME LA INFORMACIÓN DE TODA LA CADENA DE BLOQUES
+console.log(JSON.stringify(Redes2Coin,null,2)); 
+
+
+
+
+//EL METODO isValid() ES PARA VERIFICAR LA INTEGRIDAD DE LOS DATOS
+console.log(Redes2Coin.isValid());
+Redes2Coin.chain[1].data ='fake data';
+console.log(Redes2Coin.isValid());
+
+
